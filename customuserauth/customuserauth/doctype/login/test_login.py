872f2775f77b1598505bfd8b0f09add8b2759dff
SamWaku <SamuelWaku1st@gmail.com>
2023-01-19 01:53:49 +0200

    initial commit stuff

---
+++ customuserauth/customuserauth/doctype/login/test_login.py
@@ -0,0 +1,9 @@
+# Copyright (c) 2023, samuel wakumelo and Contributors
+# See license.txt
+
+# import frappe
+from frappe.tests.utils import FrappeTestCase
+
+
+class TestLogin(FrappeTestCase):
+	pass
